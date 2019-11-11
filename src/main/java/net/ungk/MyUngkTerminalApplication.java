06356b3ba3ded568f6664fd2580ef345e85596f3
Gunwoong-Heo <gjrjsdnd1129@gmail.com>
2019-11-11 17:01:34 +0900

    initial commit

---
+++ src/main/java/net/ungk/MyUngkTerminalApplication.java
@@ -0,0 +1,13 @@
+package net.ungk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUngkTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUngkTerminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ungk/MyUngkTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ungk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUngkTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
